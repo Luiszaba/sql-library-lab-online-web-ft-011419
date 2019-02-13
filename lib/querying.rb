a8b41d8e797fc130550112aec3a6708aa6003d67
Luis Zabala <luis.zabala076@gmail.com>
2019-02-13 23:50:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(*) FROM characters.species GROUP BY characters.species;"
+  "SELECT character.species, COUNT(*) FROM character.species GROUP BY character.species;"
 end
 
 def select_name_and_series_subgenres_of_authors
